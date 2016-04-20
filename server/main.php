0a0e3143cb4366480377bb11aac389a917400bf9
Richard Adenling <dreeze@gmail.com>
2016-04-20 20:33:50 +0200

    Move server to separate directory

--- server/main.php
+++ server/main.php
@@ -1,9 +1,9 @@
 <?php
+
 spl_autoload_extensions(".php");
 spl_autoload_register();
 
-require_once("vendor/autoload.php");
-use Devristo\Phpws\Server\WebSocketServer;
+require_once("../vendor/autoload.php");
 
 function main() {
 	$loop = \React\EventLoop\Factory::create();
